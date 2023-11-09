c274db3aa868430b49947c84f5455ac7a10dd05b
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2023-11-09 18:24:41 +0600

    Added About Page And Modify Needed Code!

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ function openMenu() {
 function closeMenu() {
   sideMenu.style.right = "-200px";
 }
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
